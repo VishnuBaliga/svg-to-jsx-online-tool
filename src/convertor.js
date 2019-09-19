8b02280ec3918228c58fc6f89bb96ccaab84501b
Vishnu Baliga <vishnubaliga@Baligas-MacBook-Pro.local>
2019-09-19 19:46:46 +0530

    styles updated

--- src/convertor.js
+++ src/convertor.js
@@ -3,14 +3,16 @@ import replaceOnce from 'replace-once';
 import { find, replace } from './findAndReplace';
 import copy from 'copy-to-clipboard';
 
-
+ 
 import SyntaxHighlighter from 'react-syntax-highlighter';
-import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+import { dracula } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
 import svgtojsx from 'svg-to-jsx'; 
+ 
 
 var sampleSvg = '<svg version="1.1"><path id="myPath" style="font-family: Verdana; margin-bottom: 10px; -webkit-transition: all; ms-transition: all;"/></svg>';
  
+
 export default class Convertor extends React.Component{
     constructor(){
         super();
@@ -20,8 +22,11 @@ export default class Convertor extends React.Component{
             loading: false,
             copying:false,
             error:false,
+            output: false
         }
+        this.inputRef = React.createRef();
     }
+ 
       handleCopy = (value) => {
             copy(value);
         console.log("copieeedd",  value);
@@ -36,15 +41,17 @@ export default class Convertor extends React.Component{
     }
     handleChange = (e) =>{  
     
-        const {rawData} = this.state;
+        const rawData = this.inputRef.current.value;
 
+            this.setState({
+                rawData
+            });    
         if(rawData){
+
             this.setState({
                 loading: true,
                 error:false,
-            });   
-            // const FinalData =  async () => { return replaceOnce(rawData, find, replace, 'gi')};
-            // const convertedData = await FinalData();
+            });    
 
             svgtojsx(rawData, (error, jsx) => {
                 if(jsx) {
@@ -52,16 +59,17 @@ export default class Convertor extends React.Component{
                         this.setState({ 
                             convertedData: jsx, 
                             loading: false,
+                            output: true
                         });   
                     },500);
                 }
-                else{
-                    // console.log("Error-->>",error,"------->>>",typeof(error));
+                else{ 
                     setTimeout(()=>{
                         this.setState({ 
                                 convertedData: `${error}`, 
                         error:true,
                         loading: false,
+                        output: true
                         });   
                     },500); 
                 }
@@ -77,36 +85,36 @@ export default class Convertor extends React.Component{
                     loading: false,
                     copying:false,
                     error:false,
+                    output: false
                     }); 
         }
 
     }
     render()
     {
-        const {convertedData,loading, rawData,copying,error} = this.state;
+        const {convertedData,loading, rawData,copying,error, output} = this.state;
         return(
             <>
-            <h1>SVG to JSX Converter</h1>
-             
-                <textarea placeholder={'Paster your SVG here..'} id="textarea" value={rawData} onChange={(e)=>this.setState({rawData: e.target.value}) } rows={'10'}  /><br/>
-                <button type={'submit'} disabled={!rawData} onClick={this.handleChange}>{loading?'Converting...':'Convert SVG to JSX'}</button> 
-            <br/>
-            <br/>
-            <br/>
-            {/* {loading?<>loading....<div className="loader"></div></>:null}  */}
+            <header>
+                <h3>SVG to JSX Converter</h3> 
+            </header>
+ 
+                <div className="input-container">
+                    <h2>Input</h2>
+                    <textarea placeholder={'Paster your SVG here..'} value={rawData} ref={this.inputRef} onChange={(e)=>this.handleChange(e) } rows={'10'}  /><br/>
+                    <button type={'submit'} disabled={!rawData|loading} onClick={this.handleChange}>{loading?'Converting...':'Convert SVG to JSX'}</button> 
+                </div> 
 
+    {output && rawData &&
             <div className={(error) ? 'error output-container':'output-container'}>
-                {!error && rawData && convertedData && <button onClick={e=>this.handleCopy(`${convertedData}`)}>{copying?'Copied!':'Copy Code'}</button> }
-                {/* <textarea ref="output" rows={'10'} value={convertedData} disabled />  */}
+                <h2>Output</h2>
+                {!error && rawData && convertedData && <button onClick={e=>this.handleCopy(`${convertedData}`)}>{copying?'Copied!':'Copy Code'}</button> } 
                 
-                
-                 <SyntaxHighlighter language="javascript" style={docco}>
+                 <SyntaxHighlighter language="javascript" style={dracula}>
                     {convertedData}
-                </SyntaxHighlighter>
-                
-
-    
+                </SyntaxHighlighter> 
             </div>
+    }
 
            
 
